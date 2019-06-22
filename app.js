01a836b8fb47bc0713cd7aabde37bc4af4cf8f45
julio-jimenez <juliofrancisj@gmail.com>
2019-06-22 13:22:04 -0400

    add env port

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use(function(req, res) {
 });
 
 
-const server = app.listen(3000, () => {
+const port =  process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
   const host = server.address().address;
   const { port } = server.address();
 
